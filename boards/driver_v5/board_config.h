a373212970e94283ae16e8d01d92837664a24b14
BFFonsecaYnv <bruno.fonseca@ynvisible.com>
2024-12-17 18:53:57 +0000

    Add custom USB PID
     - Add Microchip VID: 0x04d8
     - Add Driver v5 PID: 0xe538

--- boards/driver_v5/board_config.h
+++ boards/driver_v5/board_config.h
@@ -7,8 +7,8 @@
 
 #define BOARD_ID "SAMD21J18A-Driver-v5"
 
-//#define USB_VID 0x2341
-//#define USB_PID 0x024D
+#define USB_VID 0x04d8
+#define USB_PID 0xe538
 
 #define LED_PIN PIN_PA22        // LED_R
 #define LED_TX_PIN PIN_PB15     // LED_B
